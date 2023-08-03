23186e781c3f27eab55c7183dd7c278f5ce0ebb5
Anna Sperkach <annsperkach@gmail.com>
2023-08-03 12:51:10 +0300

    Implementing the main view for game and lobby pages

--- frontend/src/app/components/game/game.component.ts
+++ frontend/src/app/components/game/game.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.css']
 })
-export class GameComponent {
+export class GameComponent implements OnInit{
 
+  constructor () { }
+
+  ngOnInit(): void {
+  }
+
+  role = "operative";
 }
